941153b3a8cfc7d36193d2aa935f8b31caa71353
mars1198 <emperie2006@yandex.ru>
2018-04-18 13:46:23 +0400

    Create hellow_world.py

---
+++ hellow_world.py
@@ -0,0 +1,9 @@
+def say_hello():
+    """Use the print function to say 'hello world'."""
+    # once you created your function, you can delete the line below this one.
+    print('hello world')
+
+if __name__ == "__main__":
+    # if you want to do additional testing, you can do so here. Everything here
+    # will not be run by pytest.
+    say_hello()
