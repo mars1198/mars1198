2f661ce0296b3137f080196b1e42aae48a02edb3
mars1198 <emperie2006@yandex.ru>
2018-05-02 23:58:06 +0400

    Create is_the_point_in_the_square.cpp

---
+++ is_the_point_in_the_square.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+int main ( )
+{float X,Y;
+cout<<" X = " ; cin>>X;
+cout<<" Y = " ; cin>>Y;
+if (X>=-1 && X<=3 && Y>=-2 && Y<=4)
+cout <<"the point lies in the square"<< endl ;
+else
+cout<<"the point doesn't' lie in the square"<<endl ;
+return 0;
+}
