a7a3073f954cf7625c9856ad7049e4d4ea455e92
mars1198 <emperie2006@yandex.ru>
2018-09-07 13:08:44 +0300

    Create XXX.py

---
+++ XXX.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  7 12:37:50 2018
+
+@author: Admin
+"""
+
+import random
+
+def XXXX():
+
+        a = random.randint(1, 50)
+        if a == 1:
+            print ("I dare you to make-out with me, without feeling me up, for ten minutes straight.")
+        if a == 2:
+            print ("I dare you to tease me by going down on me for as long as you can without making me orgasm.")
+        if a == 3:
+            print ("I dare you to whisper the naughtiest thing you can think of into my ear. ")
+        if a == 4:
+            print ("I dare you to go online and order us the kinkiest sex toy you can find.")
+        if a == 5:
+            print ("I dare you to unhook my bra with one hand.")
+        if a == 6:
+            print ("I dare you to masturbate to a picture of me that you have stored in your phone.")
+        if a == 7:
+            print ("I dare you to fuck me on the nearest surface we can find.")
+        if a == 8:
+            print ("I dare you to cover one of your body parts in whipped cream, and then let me lick it off of you.")
+        if a == 9:
+            print ("I dare you to go in the bathroom and take the sexiest nudes you can and then send them to me.")
+        if a == 10:
+            print ("I dare you to take erotic pictures of me while I suck your dick.")
+        if a == 11:
+            print ("I dare you to make me as horny as you possibly can without touching me.")
+        if a == 12:
+            print ("I dare you to put on a porno I can watch while you eat me out.")
+        if a == 13:
+            print (" I dare you to try on my underwear.")
+        if a == 14:
+            print ("I dare you to spank me as hard as you possibly can.")
+        if a == 15:
+            print ("I dare you to handcuff me to the bed and have your way with me.")
+        if a == 16:
+            print ("I dare you to use one of my sex toys on your own body.")
+        if a == 17:
+            print ("I dare you to do as many push-ups as you can. While naked.")
+        if a == 18:
+            print ("I dare you to kiss me anywhere, except on my lips.")
+        if a == 19:
+            print ("I dare you to fuck me in a position we’ve never tried before.")
+        if a == 20:
+            print ("I dare you to send me the dirtiest sext you can come up with.")
+        if a == 21:
+            print ("I dare you to make me orgasm, just by using your hands.")
+        if a == 22:
+            print ("I dare you to guess what color underwear I’m wearing.")
+        if a == 23:
+            print ("I dare you to be as loud as you can when you fuck me tonight.")
+        if a == 24:
+            print ("I dare you to rub your dick against my clit for as long as you can take without entering me.")
+        if a == 25:
+            print ("I dare you to sketch a picture of me naked.")
+        if a == 26:
+            print ("I dare you to bring me into the living room and fuck me in the middle of floor.")
+        if a == 27:
+            print ("I dare you to go skinny dipping with me.")
+        if a == 28:
+            print ("I dare you to blindfold me, and then kiss me somewhere I wouldn’t expect.")
+        if a == 29:
+            print ("I dare you to watch me undress without getting a hard-on.")
+        if a == 30:
+            print (" I dare you to take all of your clothes off, and keep them off for the rest of the game.")
+        if a == 31:
+            print ("I dare you to strike the most seductive pose that you can.")
+        if a == 32:
+            print ("I dare you to remove my underwear with your teeth.")
+        if a == 33:
+            print ("I dare you to turn on the sexiest song on your iPod and give me a strip tease.")
+        if a == 34:
+            print (" I dare you to keep all of the windows and doors open while you fuck me.")
+        if a == 35:
+            print (" I dare you to make me orgasm before you orgasm tonight.")
+        if a == 36:
+            print ("I dare you to watch me masturbate for as long as you can before grabbing me and fucking me yourself.")
+        if a == 37:
+            print ("I dare you to make the most authentic orgasm sounds that you can.")
+        if a == 38:
+            print ("I dare you to have sex with me for at least a half-hour before you orgasm.")
+        if a == 39:
+            print ("I dare you to run your tongue across any area of my body that you choose.")
+        if a == 40:
+            print ("I dare you to search through your closet and put on the sexiest item of clothing you own.")
+        if a == 41:
+            print (" I dare you to put on your favorite porno, so I can see what you’re into.")
+        if a == 42:
+            print ("I dare you to turn me on as much as you can by removing only one piece of your clothing.")
+        if a == 43:
+            print (" I dare you to try your best to orgasm at the same time as me tonight.")
+        if a == 44:
+            print ("I dare you to undress, and let me take a body shot off of you.")
+        if a == 45:
+            print ("I dare you to write me an erotic story about what you want to do to me.")
+        if a == 46:
+            print ("I dare you to masturbate at the same time as me.")
+        if a == 47:
+            print (" I dare you to kiss me on your favorite area of my body.")
+        if a == 48:
+            print (" I dare you to eat a banana as seductively as you can.")
+        if a == 49:
+            print ("I dare you to role play as a celebrity who wants to bang me.")
+        if a == 50:
+            print ("I dare you to make me orgasm harder than you ever have before.")
+        inp()
+
+def inp():
+    b = input("Please enter ROLL to choose dare ")
+    if b =="ROLL":
+        XXXX()
+        
+if __name__ == '__main__':
+    inp()
+    
